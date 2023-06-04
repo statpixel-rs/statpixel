19b47be6bb4713112a253032b9ccb188fc411f48
matteopolak <matteopolak@hotmail.com>
2023-06-03 23:34:44 -0400

    fix: version

--- api/src/player_old/data.rs
+++ api/src/player_old/data.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 
 use crate::minutes::Minutes;
 
-pub const VERSION: i16 = 4;
+pub const VERSION: i16 = 3;
 
 #[derive(Deserialize, bincode::Encode, bincode::Decode)]
 pub struct Data {
