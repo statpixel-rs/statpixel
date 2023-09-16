fb45303abf5839dc1aee4929852a9d9e827357ee
matteopolak <matteopolak@hotmail.com>
2023-09-16 19:39:38 -0400

    fix(ci): remove macos build, add runtime_env

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -15,7 +15,14 @@ pub type PostgresPool = Pool<AsyncPgConnection>;
 pub fn get_pool(max_size: usize) -> PostgresPool {
 	use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 
+	#[cfg(not(feature = "runtime_env"))]
 	let url = dotenvy_macro::dotenv!("DATABASE_URL");
+
+	#[cfg(feature = "runtime_env")]
+	let url = std::env::var("DATABASE_URL")
+		.expect("DATABASE_URL not set")
+		.as_str();
+
 	let manager = AsyncDieselConnectionManager::<AsyncPgConnection>::new(url);
 
 	Pool::builder(manager)
